9b90d21f16ddd9efcc6a53b0e7c2c20fa399f0b6
Ajnas Muhammed <ajnasajs44@gmailcom>
2025-05-27 13:06:04 +0530

    v5

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ const { v4: uuidv4 } = require('uuid');
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL:'https://lapcart.shop/auth/google/callback'
 },
 
 
